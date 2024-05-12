6e56cf27568324bf7bf62df875974d4f036a0817
hlamhuy <hlamhuy@icloud.com>
2024-05-11 22:00:13 -0500

    part 1a completed

---
+++ part1/src/App.jsx
@@ -0,0 +1,54 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]} exercise={props.exercises[0]} />
+      <Part part={props.parts[1]} exercise={props.exercises[1]} />
+      <Part part={props.parts[2]} exercise={props.exercises[2]} />
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.exercises[0] + props.exercises[1] + props.exercises[2]}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    "Fundamentals of React",
+    "Using props to pass data",
+    "State of a component",
+  ];
+  const exercises = [10, 7, 14];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} exercises={exercises} />
+      <Total exercises={exercises} />
+    </div>
+  );
+};
+
+export default App;
