aa73e50e42c254d6253820e070255c729c320d04
hlamhuy <hlamhuy@icloud.com>
2024-05-28 00:16:14 -0500

    2.11 completed

---
+++ part2/2.11/src/App.jsx
@@ -0,0 +1,103 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+
+  const addName = (event) => {
+    event.preventDefault();
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook`);
+      return;
+    }
+    const nameObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    };
+
+    setPersons(persons.concat(nameObject));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const personsToShow = newFilter
+    ? persons.filter((person) =>
+        person.name.toLowerCase().includes(newFilter.toLowerCase())
+      )
+    : persons;
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter value={newFilter} onChange={handleFilterChange} />
+      <h3>Add new contact</h3>
+      <PersonForm
+        onSubmit={addName}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h3>Numbers</h3>
+      <Persons personsToShow={personsToShow} />
+    </div>
+  );
+};
+
+const PersonForm = ({
+  onSubmit,
+  newName,
+  handleNameChange,
+  newNumber,
+  handleNumberChange,
+}) => (
+  <form onSubmit={onSubmit}>
+    <div>
+      name: <input value={newName} onChange={handleNameChange} />
+    </div>
+    <div>
+      number: <input value={newNumber} onChange={handleNumberChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+);
+const Persons = ({ personsToShow }) => (
+  <div>
+    {personsToShow.map((person) => (
+      <div key={person.id}>
+        {person.name} {person.number}
+      </div>
+    ))}
+  </div>
+);
+const Filter = ({ value, onChange }) => (
+  <div>
+    filter shown with <input value={value} onChange={onChange} />
+  </div>
+);
+
+export default App;
