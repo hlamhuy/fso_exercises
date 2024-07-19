2ab7ccf9259ecabcebc231147888567faabdec56
Huy Hoang <hlamhuy@icloud.com>
2024-07-19 17:29:42 -0500

    bugfix part3

---
+++ part3/3.1/README.md
@@ -0,0 +1,1 @@
+Visit your newly deployed app at https://huy-fso-phonebook.fly.dev/
--- part3/3.1/app.js
+++ part3/3.1/app.js
@@ -14,10 +14,10 @@ logger.info("connecting to", config.MONGODB_URI);
 mongoose
   .connect(config.MONGODB_URI)
   .then((result) => {
-    console.log("connected to MongoDB");
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
+    logger.error("error connecting to MongoDB:", error.message);
   });
 
 app.use(cors());
