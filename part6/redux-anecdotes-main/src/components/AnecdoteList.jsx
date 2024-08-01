097a257adb9af9bb12c90158d75a22f81ac92f2c
Huy Hoang <hlamhuy@icloud.com>
2024-08-01 13:42:55 -0500

    6.15 completed

--- part6/redux-anecdotes-main/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes-main/src/components/AnecdoteList.jsx
@@ -1,18 +1,23 @@
 import { useSelector, useDispatch } from "react-redux";
 import { voteAnecdote } from "../reducers/anecdoteReducer";
-
+import { showNotification } from "../reducers/notificationReducer";
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector(({ anecdotes, filter }) =>
+    anecdotes.filter((anecdote) =>
+      anecdote.content.toLowerCase().includes(filter.toLowerCase())
+    )
+  );
   const dispatch = useDispatch();
 
   const vote = (id) => {
-    console.log("vote", id);
+    const anecdote = anecdotes.find((a) => a.id === id);
     dispatch(voteAnecdote(id));
+    dispatch(showNotification(`You voted '${anecdote.content}'`, 5));
   };
 
   return (
     <div>
-      {anecdotes
+      {[...anecdotes]
         .slice()
         .sort((a, b) => b.votes - a.votes)
         .map((anecdote) => (
