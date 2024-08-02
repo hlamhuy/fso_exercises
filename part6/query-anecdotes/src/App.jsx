ca7cea4f391a1cdf502e6a822b605aa2815dd6c8
Huy Hoang <hlamhuy@icloud.com>
2024-08-02 15:02:43 -0500

    6.22 completed

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,21 +1,32 @@
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getAnecdotes, updateAnecdote } from "./requests";
 
 const App = () => {
-  const handleVote = (anecdote) => {
-    console.log("vote");
-  };
+  const queryClient = useQueryClient();
 
+  const voteAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+    },
+  });
   const result = useQuery({
     queryKey: ["anecdotes"],
-    queryFn: () =>
-      axios.get("http://localhost:3001/anecdotes").then((res) => res.data),
+    queryFn: getAnecdotes,
+    retry: 1,
   });
+
+  const handleVote = (anecdote) => {
+    voteAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
+  };
   console.log(JSON.parse(JSON.stringify(result)));
   if (result.isLoading) {
     return <div>loading data...</div>;
+  }
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>;
   }
 
   const anecdotes = result.data;
--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/notes";
+const baseUrl = "http://localhost:3001/anecdotes";
 
 export const getAnecdotes = () => axios.get(baseUrl).then((res) => res.data);
 
 export const addAnecdote = (newAnecdote) =>
   axios.post(baseUrl, newAnecdote).then((res) => res.data);
+
+export const updateAnecdote = (votedAnecdote) =>
+  axios
+    .put(`${baseUrl}/${votedAnecdote.id}`, votedAnecdote)
+    .then((res) => res.data);
