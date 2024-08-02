9e6c07d9200f90d2de53ae1c6dfb66ab1d70b9bb
Huy Hoang <hlamhuy@icloud.com>
2024-08-02 14:30:26 -0500

    6d added

---
+++ part6/query-anecdotes/src/requests.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001/notes";
+
+export const getAnecdotes = () => axios.get(baseUrl).then((res) => res.data);
+
+export const addAnecdote = (newAnecdote) =>
+  axios.post(baseUrl, newAnecdote).then((res) => res.data);
