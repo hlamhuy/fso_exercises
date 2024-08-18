952092088a61973a8e5834b39fe326eff3e16352
Huy Hoang <hlamhuy@icloud.com>
2024-08-18 17:45:48 -0500

    9.22 - updated patient entry data

--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -4,7 +4,32 @@ export type DiagnosisType = {
   latin?: string;
 };
 
-export interface Entry {}
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnosisType["code"]>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: discharge;
+}
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: sickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Patient {
   id: string;
@@ -24,4 +49,21 @@ export enum Gender {
   Male = "male",
   Female = "female",
   Other = "other",
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface sickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface discharge {
+  date: string;
+  criteria: string;
 }
