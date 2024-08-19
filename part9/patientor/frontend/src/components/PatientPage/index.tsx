e009d8da3d8c78636916f8c6a136294bdbad30e6
Huy Hoang <hlamhuy@icloud.com>
2024-08-18 20:54:58 -0500

    9.23 - added entries to patient page

--- part9/patientor/frontend/src/components/PatientPage/index.tsx
+++ part9/patientor/frontend/src/components/PatientPage/index.tsx
@@ -10,6 +10,21 @@ const PatientPage = ({ patient }: Props) => {
       <p>gender: {patient?.gender}</p>
       <p>ssn: {patient?.ssn}</p>
       <p>occupation: {patient?.occupation}</p>
+      <h3>entries</h3>
+      <p>
+        {patient?.entries.map((entry) => (
+          <div key={entry.id}>
+            <p>
+              {entry.date}: {entry.description}
+            </p>
+            <ul>
+              {entry.diagnosisCodes?.map((d) => (
+                <li key={d}>{d}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </p>
     </div>
   );
 };
--- part9/patientor/frontend/src/types.ts
+++ part9/patientor/frontend/src/types.ts
@@ -7,7 +7,7 @@ export interface Diagnosis {
 export enum Gender {
   Male = "male",
   Female = "female",
-  Other = "other"
+  Other = "other",
 }
 
 export interface Patient {
@@ -17,6 +17,48 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: discharge;
+}
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: sickLeave;
+}
+interface sickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface discharge {
+  date: string;
+  criteria: string;
+}
