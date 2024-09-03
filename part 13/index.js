248ad78a5a0f3358125c0516306f00a1afe1f916
Huy Hoang <hlamhuy@icloud.com>
2024-09-03 10:52:10 -0500

    13.16 - added /api/authors

--- part 13/index.js
+++ part 13/index.js
@@ -8,15 +8,17 @@ const { connectToDatabase } = require('./util/db');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
+const authorsRouter = require('./controllers/authors');
 
 app.use(express.json());
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
+app.use('/api/authors', authorsRouter);
 
 app.use((err, req, res, next) => {
   if (err.name === 'SequelizeValidationError') {
-    const errors = err.errors.map((e) => e.message); 
+    const errors = err.errors.map((e) => e.message);
     return res.status(400).json({ error: errors });
   }
   next(err);
