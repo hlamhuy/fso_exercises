2a8192edab75e64c5c0b471e83090e42027be2f2
Huy Hoang <hlamhuy@icloud.com>
2024-09-04 18:27:08 -0500

    13.19 - added reading lists

--- part 13/models/index.js
+++ part 13/models/index.js
@@ -1,12 +1,17 @@
 const Blog = require('./blog');
 const User = require('./user');
+const ReadingLists = require('./readingLists');
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
+
+User.belongsToMany(Blog, { through: ReadingLists, as: 'listed_blog' });
+Blog.belongsToMany(User, { through: ReadingLists, as: 'users_listed' });
 //Blog.sync({ alter: true });
 //User.sync({ alter: true });
 
 module.exports = {
   Blog,
   User,
+  ReadingLists,
 };
