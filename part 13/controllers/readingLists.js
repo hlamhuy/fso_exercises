8bb90f467cc04c4a4003073bbadae59cfa0f9523
Huy Hoang <hlamhuy@icloud.com>
2024-09-04 21:54:42 -0500

    13.22 - added put request to change read status

--- part 13/controllers/readingLists.js
+++ part 13/controllers/readingLists.js
@@ -1,9 +1,22 @@
 const router = require('express').Router();
-const { ReadingLists } = require('../models');
+const { User, ReadingLists } = require('../models');
+const { tokenExtractor } = require('../util/middleware');
 
 router.post('/', async (req, res) => {
   const list = ReadingLists.create(req.body);
   res.json(list);
+});
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id);
+  const blog = await ReadingLists.findByPk(req.params.id);
+  if (user && blog && req.body.read !== undefined && user.id === blog.userId) {
+    blog.read = req.body.read;
+    await blog.save();
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
